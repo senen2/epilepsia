2b220f0eb8ecd8e550ec184be418640887f4fcc1
senen2 <senenbotello@gmail.com>
2016-11-23 18:27:50 -0500

    nov 23 night

--- convc/params.py
+++ convc/params.py
@@ -10,7 +10,7 @@ def param(patient):
                , "cv2_size": 5
                , "cv1_channels": 4
                , "cv2_channels": 8
-               , "hidden": 25
+               , "hidden": 4
                , "img_resize": 16
                , "learning_rate": 0.0005
                , "dropout": 0.5
--- convc/train.py
+++ convc/train.py
@@ -177,6 +177,19 @@ epochs learning_rate cv1_size cv2_size cv1_channels cv2_channels hidden img_resi
     10000      0.005        5       5          16            8        4        16      0.5
 Accuracy: 1.0 Accuracy test: 0.929398
 AUC 1.0 AUC test 0.939593947815 patient 3
+
+Accuracy: epochs learning rate cv1 size cv2 size cv1 channels cv2channels hidden img resize dropout
+0.964004 1000 0.005 5 5 4 4 4 16 0.5
+AUC 0.98162107207 Cost 0.27 patient 3 con todo, 4
+
+Accuracy: epochs learning rate cv1 size cv2 size cv1 channels cv2channels hidden img resize dropout
+0.989373 3000 0.005 5 5 4 4 4 16 0.5
+AUC 0.992125984252 Cost 0.237044 patient 3 con todo
+
+Accuracy: epochs learning rate cv1 size cv2 size cv1 channels cv2channels hidden img resize dropout
+0.988344 3000 0.005 5 5 4 4 4 16 0.5
+AUC 0.991076115486 Cost 0.328393 patient 3 con todo
+
 '''
 import tensorflow as tf
 import numpy as np
@@ -190,17 +203,17 @@ print "begin"
 patient = 3
 group = "train_%s_new" % patient
 parameters = param(patient)
-training_epochs = 1000
+training_epochs = 3000
 
-images, labels, names = read_images(group)
+#images, labels, names = read_images(group)
 #print images.shape, labels.shape, len(names)
-images, labels, names = read_images_balanced(group, 2)
+images, labels, names = read_images_balanced(group, 4)
 print images.shape, labels.shape, len(names)
 features, prob, acc, cost = train_tf(images, labels, parameters, training_epochs=training_epochs)
 
 print "Accuracy:", "epochs", "learning rate", "cv1 size", "cv2 size", "cv1 channels", "cv2channels", "hidden", "img resize", "dropout"
 print acc, training_epochs, parameters["learning_rate"], parameters["cv1_size"], parameters["cv2_size"], parameters["cv1_channels"], parameters["cv2_channels"], parameters["hidden"], parameters["img_resize"], parameters["dropout"]
-print "AUC", auc(labels, prob), "patient", patient, "con todo"
+print "AUC", auc(labels, prob), "Cost", cost, "patient", patient, "con todo"
 
 scipy.io.savemat("resp_%s_new" % patient, features, do_compression=True)    
 print "end"
